67e8f7f8763f549d86e6a8a093d2d50ea41693e9
a.gribul <a.gribul@tinkoff.ru>
2023-04-04 13:34:48 +0300

    fix: Added check for no changes in the code.

--- commitmsg/main.py
+++ commitmsg/main.py
@@ -9,6 +9,9 @@ def main():
     git_branch = repo.active_branch.name
     git_branch = "" if git_branch == "master" else f"{git_branch}: "
     diff = repo.git.diff(repo.head.commit.tree)
+    if len(diff) == 0:
+        print("No changes detected.")
+        return
     type, message = generator(diff)
     print("Generated commit message:\n\n")
     print(f"{type}: {git_branch}{message}")
